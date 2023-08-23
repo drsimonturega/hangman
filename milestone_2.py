21f8c91532564a8c3deae4a59267de84606b45a1
drsimonturega <drsimonturega@gmail.com>
2023-08-23 12:51:53 +0100

    milestone_2.py refactored and optimised.

--- milestone_2.py
+++ milestone_2.py
@@ -1,18 +1,26 @@
 # Defining varibles for hangman game
 import random
-word_list = ['apple', 'pear', 'blackcurrent', 'raspberry', 'mango']
-#print(word_list[random.randrange(4)])
-word = random.choice(word_list)
-print(f'{word}')
 
-guess = input('Enter a letter please, ')
+# Defining a fuction that selects a word from a list at random
+def sel_word():
+	# Defining a list of favorite fruits
+	word_list = ['apple', 'pear', 'blackcurrent', 'raspberry', 'mango']
+	word = random.choice(word_list)
+	print(f'{word}')
+	return word
 
-if len(guess) == 1 and guess.isalpha() == True:
-	print('Good guess!')
-else:
-	print('Oops! That is not a valid input.')
+# Defining a function that takes a use input checks that is is a single chrater and alphabetical
+def inp_guess():
+	guess = input('Enter a letter please, ')
+	if len(guess) == 1 and guess.isalpha() == True:
+		print('Good guess!')
+	else:
+		print('Oops! That is not a valid input.')
+	return guess
 
-
-
-print(guess)
+# Tempory code to check the above functions will need to think abaout making varibles global
+sel_word()
+sel_word()
+#inp_guess()
+print(f'{inp_guess()}')
 
