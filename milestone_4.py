9e374a3cc32d000137e9e10904a1716d22d4bfdc
Simon <simon@UNKNOWN.Home>
2023-10-09 17:45:56 +0100

    Milestone_4.py has Docstrings added  and a de_bug jupyter notebook

--- milestone_4.py
+++ milestone_4.py
@@ -12,17 +12,26 @@ class Hangman:
     num_letters = int()
 #
     def __init__(self, word_list, num_lives = 5):
+        """
+        Inintailises the class varibles bound to an instance of the class
+        """
         self.word_list = word_list
         self.num_lives = num_lives
 
     #choseing word from inputted list
     def sel_word(self):
+        """
+        Selectes a word from the word list input, and sets up Hangman.word_guess list
+        """
         Hangman.word = random.choice(self.word_list)
         Hangman.word_guessed = [str("_") for n in range(0,len(Hangman.word))]
         return Hangman.word
 
     # A function that checks weather the letter in the string parsed from ask_for_input() is in a string word.
     def check_guess(self, guess):
+        """
+        Checks weather the guess letter is in the word selected, manages num_letters and num lives.
+        """
         Hangman.num_letters = len(Hangman.word)
         guess = guess.lower()
         if guess in Hangman.word:
@@ -42,6 +51,9 @@ class Hangman:
     
     # collect and validate input
     def ask_for_input(self):
+        """
+        Collects an input letter and chec its validity as a single letter of the alphabet.
+        """
         col_inp_guess = True
         while col_inp_guess == True:
             guess = input('Enter a letter please, ')
